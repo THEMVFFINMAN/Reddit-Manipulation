b47325b3b71d5d9f5e5cb167109e889eb81fe654
Sean Beck <seanmckaybeck@gmail.com>
2015-09-29 21:52:50 -0400

    removed vote_test script

--- vote_test.py
+++
@@ -1,7 +0,0 @@
-from manipulator import utils
-
-c = utils.RedAPI()
-c.login('nigguhnigguhnigguh', 'pooppoop')
-# change the first value to up, down, or null the vote for
-# https://www.reddit.com/r/learnpython/comments/3mqphh/learner_here_please_send_help_p/cvhfg0h
-c.vote(1, 't1_cvhfg0h', 'learnpython')
