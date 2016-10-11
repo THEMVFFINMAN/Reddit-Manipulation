409287d00a09ea81a82a2a80579f9c4c492d7cd2
JJ <joshuajordanlowe@gmail.com>
2016-10-11 19:40:44 -0400

    Unsubscribes from all reddit subs
    Added a second delay because even manually clicking too fast can cause it not to register.

--- MakingNewAccount/UnsubscribeAll.js
+++ MakingNewAccount/UnsubscribeAll.js
@@ -1,1 +1,7 @@
-test
+time = 1000
+$(".option.active.remove.login-required").each(function() {
+    time += 1000
+    window.setTimeout(function() {
+        $($(".option.active.remove.login-required")[0]).click();
+    }, time);
+});
