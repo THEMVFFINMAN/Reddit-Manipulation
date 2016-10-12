e61a567ede7a893ef72a109b0a8a048e78a0d4f1
JJ <joshuajordanlowe@gmail.com>
2016-10-12 12:37:17 -0400

    Update Subscribe.js

--- MakingNewAccount/Subscribe.js
+++ MakingNewAccount/Subscribe.js
@@ -1,5 +1,3 @@
-https://www.reddit.com/r/army+bestof+bodybuilding+books+bugout+creepy+cringe+cringepics+DIY+Firearms+Fitness+fitnesscirclejerk+GameDeals+guineapigs+gundeals+guns+hacking+lockpicking+metasploit+Military+MilitaryGfys+milsurp+movies+nationalguard+netsec+patientgamers+preppers+programming+PropagandaPosters+Python+techsupportgore+television+videos+weightroom+WTF
-
 time = 1000
 $(".option.active.add.login-required").each(function() {
     time += 1000
