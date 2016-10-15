9cc3d359aedf7b753f0c5831222d3b8abb05611a
JJ <joshuajordanlowe@gmail.com>
2016-10-15 16:49:53 -0700

    Got too used to python

--- MakingNewAccount/UnsubscribeAll.js
+++ MakingNewAccount/UnsubscribeAll.js
@@ -1,6 +1,6 @@
-time = 1000
+time = 1000;
 $(".option.active.remove.login-required").each(function() {
-    time += 1000
+    time += 1000;
     window.setTimeout(function() {
         $($(".option.active.remove.login-required")[0]).click();
     }, time);
